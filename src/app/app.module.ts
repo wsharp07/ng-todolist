ff772e376862b32bfdd8b2af3972c5e128de9298
William Sharp <wsharp07@gmail.com>
2020-10-09 22:48:08 -0400

    updating the add task to use the form control and to reset on submit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule } from '@angular/forms';
+
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireDatabase } from '@angular/fire/database';
 
@@ -9,6 +11,7 @@ import { TodoComponent } from './todo/todo.component';
 
 import { environment } from '../environments/environment';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -20,6 +23,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     NgbModule,
+    FontAwesomeModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,19 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
 
+import {NgForm} from '@angular/forms';
+
+import { faPlusSquare, faTrashAlt } from '@fortawesome/free-regular-svg-icons';
+import { AngularFireAction } from '@angular/fire/database/interfaces';
+
 @Component({
   selector: 'todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
 
+
 export class TodoComponent implements OnInit {
 
   items: Observable<any[]>;
   itemsRef: AngularFireList<any>;
+  faPlusSquare = faPlusSquare;
+  faTrashAlt = faTrashAlt;
 
   constructor(db: AngularFireDatabase) {
-    this.items = db.list('items').valueChanges();
+    this.items = db.list('items').snapshotChanges();
     this.itemsRef = db.list('items');
   }
 
@@ -22,6 +30,22 @@ export class TodoComponent implements OnInit {
     this.itemsRef.push({
       task: newTask
     })
+  }
+
+  removeItem(obj: AngularFireAction<any>) {
+    this.itemsRef.remove(obj.key);
+  }
+
+  onSubmit(frmAddTask: NgForm) {
+ 
+    if (!frmAddTask.valid) {
+      console.log("INVALID FORM");
+      return;
+    }
+
+    this.addItem(frmAddTask.value.newTask);
+
+    frmAddTask.resetForm();
   }
 
   ngOnInit(): void {
